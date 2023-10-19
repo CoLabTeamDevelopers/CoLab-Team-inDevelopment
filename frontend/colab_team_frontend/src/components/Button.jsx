9bfdd8c13013953aa8f03cb765ac4f457a23af00
SiddhantTotade <siddhanttotade.1994@gmail.com>
2023-10-19 21:10:40 +0530

    chore : directory

--- frontend/colab_team_frontend/src/components/Button.jsx
+++ frontend/colab_team_frontend/src/components/Button.jsx
@@ -1,7 +1,6 @@
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 
-<<<<<<< HEAD
 export default function BasicButtons({
   label,
   sx,
@@ -21,12 +20,6 @@ export default function BasicButtons({
         variant="contained"
         sx={sx}
       >
-=======
-export default function BasicButtons({ label, sx }) {
-  return (
-    <Stack spacing={2} direction="row">
-      <Button variant="contained" sx={sx}>
->>>>>>> bd9ee96446c63a3d4b21079ceac79d01f81062f1
         {label}
       </Button>
     </Stack>
--- frontend/colab_team_frontend/src/components/authentication/Container.jsx
+++ frontend/colab_team_frontend/src/components/authentication/Container.jsx
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-=======
-import * as React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
->>>>>>> bd9ee96446c63a3d4b21079ceac79d01f81062f1
 import Wave from "../../assets/svg/Wave";
 
 export default function SimpleContainer() {
