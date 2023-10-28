577109552105cce303c9e64ac33ee19d3893b66f
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2023-10-28 16:49:17 +0600

    chore: move form schemas into separate file (#14)
    Added typescript

---
+++ frontend/colab_team_frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src/'),
+    },
+  },
+});
