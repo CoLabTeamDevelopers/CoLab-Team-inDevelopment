57f6f9eea15f4e8d57f873aa894147e2311107c8
SiddhantTotade <siddhanttotade.1994@gmail.com>
2023-11-15 21:49:47 +0530

    add : Profile page

--- frontend/colab_team_frontend/src/pages/app/Profile.tsx
+++ frontend/colab_team_frontend/src/pages/app/Profile.tsx
@@ -8,40 +8,23 @@ import Footer from "@/components/footer/Footer";
 import EditUserDetails from "@/components/profile/EditUserDetails";
 import EditUserImage from "@/components/profile/EditUserImage";
 import ChangeEmail from "@/components/profile/ChangeEmail";
+import {
+  AboutUserStyle,
+  ChangeEmailBoxStyle,
+  ProfileBoxStyle,
+  UserDetailsBoxStyle,
+  UserDetailsStyle,
+  UserImageStyle,
+} from "@/styles/appStyles/ProfileStyles";
 
 export default function ProfilePage() {
   return (
     <React.Fragment>
       <NavBar />
       <AppContentLayout>
-        <Box
-          sx={{
-            width: "50%",
-            margin: "auto",
-            "@media(max-width:1024px)": {
-              width: "100%",
-            },
-          }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              "@media(max-width: 1024px)": {
-                display: "grid",
-                justifyContent: "center",
-                flexDirection: "row-reverse",
-              },
-            }}
-          >
-            <Box
-              sx={{
-                "@media(max-width:1024px)": {
-                  display: "flex",
-                  justifyContent: "center",
-                },
-              }}
-            >
+        <Box sx={ProfileBoxStyle}>
+          <Box sx={UserDetailsBoxStyle}>
+            <Box sx={UserImageStyle}>
               <Badge
                 overlap="circular"
                 sx={{ border: "1px solid #b39ddb", borderRadius: "100px" }}
@@ -57,32 +40,10 @@ export default function ProfilePage() {
                 />
               </Badge>
             </Box>
-            <Box
-              sx={{
-                width: "100%",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "end",
-                gap: "10px",
-                marginTop: "1rem",
-                "@media(max-width:1024px)": {
-                  display: "grid",
-                  alignItems: "start",
-                },
-              }}
-            >
+            <Box sx={UserDetailsStyle}>
               <EditUserDetails />
               <Typography variant="h2">SimpSid</Typography>
-              <Box
-                sx={{
-                  display: "flex",
-                  gap: "20px",
-                  "@media(max-width:1024px)": {
-                    flexDirection: "row-reverse",
-                  },
-                }}
-              >
+              <Box sx={ChangeEmailBoxStyle}>
                 {/* This link is displayed only to the users not to the viewers */}
                 <ChangeEmail />
                 {/* <Link>Change</Link> */}
@@ -100,13 +61,7 @@ export default function ProfilePage() {
               </Box>
             </Box>
           </Box>
-          <Box
-            sx={{
-              display: "grid",
-              gap: "5px",
-              marginTop: "10px",
-            }}
-          >
+          <Box sx={AboutUserStyle}>
             <Typography variant="h2">About Me</Typography>
             <Typography>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum
