80fe45f0dedcfe9544ae40bceab8788cedcf30cd
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2023-12-03 16:59:27 +0600

    chore(backend): add api endpoints
    Signed-off-by: Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>

--- backend/colabTeam_web/api/serializers.py
+++ backend/colabTeam_web/api/serializers.py
@@ -1,1 +1,37 @@
 from rest_framework import serializers
+
+from .models import Group, Membership, Post, Task
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = [
+            "title",
+            "description",
+            "user",
+            "requirements",
+            "benefits",
+            "max_contributors",
+            "category",
+            "is_closed",
+            "created_at",
+        ]
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ["name", "post", "members"]
+
+
+class MembershipSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Membership
+        fields = ["user", "group", "role", "date_joined"]
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ["name", "description", "created_by", "post", "created_at"]
--- backend/colabTeam_web/api/urls.py
+++ backend/colabTeam_web/api/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path, include
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
 
+from .views import PostViewSet
+
+router = SimpleRouter()
+router.register("posts/", PostViewSet, basename="post")
 
 app_name = "api"
 
 urlpatterns = [
+    path("", include(router.urls)),
     path("user/", include("registration_app.api.urls")),
 ]
